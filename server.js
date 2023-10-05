3df65132d8664d0b1cffa8b15e7aaff7479458f6
Legend101Zz <96632943+Legend101Zz@users.noreply.github.com>
2023-10-05 16:57:04 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const socketio = require("socket.io");
+
+const app = express();
+app.use(express.static((__dirname, "/public")));
+const expressServer = app.listen(9000);
+
+const io = socketio(expressServer);
