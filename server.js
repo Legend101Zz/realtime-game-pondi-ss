dddf4c0518c85913fdf504f4de3479464eca69f2
Legend101Zz <96632943+Legend101Zz@users.noreply.github.com>
2023-10-05 17:02:16 +0530

    basic server structure done

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const socketio = require("socket.io");
 
 const app = express();
-app.use(express.static((__dirname, "/public")));
+app.use(express.static(__dirname + "/public"));
 const expressServer = app.listen(9000);
 
 const io = socketio(expressServer);
