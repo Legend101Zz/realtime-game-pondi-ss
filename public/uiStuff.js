1fd4be6f51e154a6c969d45955b6533aeddd3d46
Legend101Zz <96632943+Legend101Zz@users.noreply.github.com>
2023-10-21 00:37:40 +0530

    canvas height and width changes

--- public/uiStuff.js
+++ public/uiStuff.js
@@ -1,1 +1,8 @@
-console.log("nunu");
+// set height and width of canvas equal to window
+
+let wHeight = window.innerHeight;
+let wWidth = window.innerWidth;
+const canvas = document.querySelector("#the-canvas");
+const context = canvas.getContext("2d");
+canvas.width = wWidth;
+canvas.height = wHeight;
