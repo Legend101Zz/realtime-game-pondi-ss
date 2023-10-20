f0d96757275cab7c38df17013497709c37badd90
Legend101Zz <96632943+Legend101Zz@users.noreply.github.com>
2023-10-21 00:42:01 +0530

    added login window

--- public/uiStuff.js
+++ public/uiStuff.js
@@ -6,3 +6,9 @@ const canvas = document.querySelector("#the-canvas");
 const context = canvas.getContext("2d");
 canvas.width = wWidth;
 canvas.height = wHeight;
+
+window.addEventListener("load", () => {
+  //on page load , open the login modal
+  const loginModal = new bootstrap.Modal(document.querySelector("#loginModal"));
+  loginModal.show();
+});
