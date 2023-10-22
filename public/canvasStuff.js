cbb83d3c55adec8c68c72005c47cba5eb9a52bb5
Legend101Zz <96632943+Legend101Zz@users.noreply.github.com>
2023-10-22 19:33:49 +0530

    added request frames

--- public/canvasStuff.js
+++ public/canvasStuff.js
@@ -8,12 +8,13 @@ player.locY = Math.floor(500 * Math.random() + 10); //vertical axis
 
 const draw = () => {
   context.beginPath();
-  context.fillStyle = "rgb(255,0,0";
-  context.arc(randomX, randomY, 10, 0, 2 * Math.PI); //random X anbd Y are center of the arc
+  context.fillStyle = "rgb(255,0,0)";
+  context.arc(player.locX, player.locY, 10, 0, 2 * Math.PI); //random X anbd Y are center of the arc
   context.fill();
   context.lineWidth = 3; // giving width to our circle
   context.strokeStyle = "rbg(0,255,0)";
   context.stroke();
+  requestAnimationFrame(draw);
 };
 
 canvas.addEventListener("mousemove", (event) => {
