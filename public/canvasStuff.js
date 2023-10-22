76536754867f1932a37f77484de8344af80eb161
Legend101Zz <96632943+Legend101Zz@users.noreply.github.com>
2023-10-22 19:30:59 +0530

    code refactor

--- public/canvasStuff.js
+++ public/canvasStuff.js
@@ -3,20 +3,18 @@ const init = () => {
   draw();
 };
 
-let randomX = Math.floor(500 * Math.random() + 10); //horizontal axis
-let randomY = Math.floor(500 * Math.random() + 10); //vertical axis
+player.locX = Math.floor(500 * Math.random() + 10); //horizontal axis
+player.locY = Math.floor(500 * Math.random() + 10); //vertical axis
 
-context.beginPath();
-context.fillStyle = "rgb(255,0,0";
-context.arc(randomX, randomY, 10, 0, 2 * Math.PI); //random X anbd Y are center of the arc
-context.fill();
-context.lineWidth = 3; // giving width to our circle
-context.strokeStyle = "rbg(0,255,0)";
-context.stroke();
-
-console.log(randomX, randomY);
-
-const draw = () => {};
+const draw = () => {
+  context.beginPath();
+  context.fillStyle = "rgb(255,0,0";
+  context.arc(randomX, randomY, 10, 0, 2 * Math.PI); //random X anbd Y are center of the arc
+  context.fill();
+  context.lineWidth = 3; // giving width to our circle
+  context.strokeStyle = "rbg(0,255,0)";
+  context.stroke();
+};
 
 canvas.addEventListener("mousemove", (event) => {
   // console.log(event)
