78bd95f36f1e18a8447e52beaa06ace81bfc4759
Legend101Zz <96632943+Legend101Zz@users.noreply.github.com>
2023-10-22 21:00:18 +0530

    created orbs class

--- socketStuff/socketMain.js
+++ socketStuff/socketMain.js
@@ -1,1 +1,5 @@
 const io = require("../server").io;
+
+const Orb = require("./classes/Orb/Orb");
+//make an orbs array that will host all 5000 not player orbs
+// every time one is absorbed the server will make a new one
