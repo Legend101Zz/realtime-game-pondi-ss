da54973cc911706e116237b84c7c46d6861404a5
Legend101Zz <96632943+Legend101Zz@users.noreply.github.com>
2023-10-22 18:43:40 +0530

    added stroke line

--- public/canvasStuff.js
+++ public/canvasStuff.js
@@ -7,10 +7,12 @@ let randomX = Math.floor(500 * Math.random() + 10); //horizontal axis
 let randomY = Math.floor(500 * Math.random() + 10); //vertical axis
 
 context.beginPath();
-context.fillStyle("rgb(0,255,0");
+context.fillStyle = "rgb(255,0,0";
 context.arc(randomX, randomY, 10, 0, 2 * Math.PI); //random X anbd Y are center of the arc
 context.fill();
-context.lineWidth = 3;
+context.lineWidth = 3; // giving width to our circle
+context.strokeStyle = "rbg(0,255,0)";
+context.stroke();
 
 console.log(randomX, randomY);
 
