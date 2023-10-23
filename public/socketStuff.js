bc48c4624ffd6338e042d3f3e195c85966de883f
Legend101Zz <96632943+Legend101Zz@users.noreply.github.com>
2023-10-23 16:17:27 +0530

    sending players mouse position from client to the server

--- public/socketStuff.js
+++ public/socketStuff.js
@@ -4,6 +4,13 @@ const init = async () => {
   const initOrbs = await socket.emitWithAck("init", {
     playerName: player.name,
   });
+  //our await has been resolved, so let's tock
+  setInterval(() => {
+    socket.emit("tock", {
+      xVector: player.xVector,
+      yVector: player.yVector,
+    });
+  }, 33);
   orbs = initOrbs;
   //called inside of start-game click handler
   console.log(orbs);
--- socketStuff/socketMain.js
+++ socketStuff/socketMain.js
@@ -25,6 +25,7 @@ initGame();
 
 io.on("connect", (socket) => {
   // a player has connected
+  let player = {};
   socket.on("init", (playerObj, callBack) => {
     if (players.length === 0) {
       //Issue an event to every connected socket , that is playing the game , 30 timees per second
@@ -42,11 +43,33 @@ io.on("connect", (socket) => {
     const playerData = new PlayerData(playerName, settings);
 
     // a master player object to house
-    const player = new Player(socket.id, playerConfig, playerData);
+    player = new Player(socket.id, playerConfig, playerData);
     players.push(player);
     // sends back orbs as acknowledge ment
     callBack(orbs);
   });
+  // The client sent over a tock
+  socket.on("tock", (data) => {
+    speed = player.playerConfig.speed;
+    const xV = (player.playerConfig.xVector = data.xVector);
+    const yV = (player.playerConfig.yVector = data.yVector);
+
+    if (
+      (player.playerData.locX < 5 && xV < 0) ||
+      (player.playerData.locX > 500 && xV > 0)
+    ) {
+      player.playerData.locY -= speed * yV;
+    } else if (
+      (player.playerData.locY < 5 && yV > 0) ||
+      (player.playerData.locY > 500 && yV < 0)
+    ) {
+      player.playerData.locX += speed * xV;
+    } else {
+      player.playerData.locX += speed * xV;
+      player.playerData.locY -= speed * yV;
+    }
+  });
+
   socket.on("disconnect", () => {
     //check to se if players is empty, then stop ticking
     if (players.length === 0) {
