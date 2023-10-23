cdecec5393bb2672f38c031c7726cef98a0c860c
Legend101Zz <96632943+Legend101Zz@users.noreply.github.com>
2023-10-23 12:58:55 +0530

    brought player,playerdata & playerconfig into the scope of the socket server

--- socketStuff/socketMain.js
+++ socketStuff/socketMain.js
@@ -1,5 +1,7 @@
 const io = require("../server").io;
-
+const Player = require("./classes/Player");
+const PlayerConfig = require("./classes/PlayerConfig");
+const PlayerData = require("./classes/PlayerData");
 const Orb = require("./classes/Orb");
 //make an orbs array that will host all 500 not player orbs
 // every time one is absorbed the server will make a new one
